202c4e4d71fb5f5e727ab80f189e8169b9638b23
YoungFlamer <serhii_iyevlyev@ukr.net>
2020-11-25 16:40:15 +0200

    Units created

---
+++ New Unity Project/Assets/Scripts/FractionMember.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FractionMember : MonoBehaviour
+{
+    [SerializeField]
+    private string unitListerTag;
+    [SerializeField]
+    public UnitLister lister;
+    [SerializeField]
+    public Fraction fraction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lister = GameObject.FindGameObjectWithTag(unitListerTag).GetComponent<UnitLister>();
+        lister.units.Add(this.gameObject);
+    }
+    private void OnDestroy()
+    {
+        lister.units.Remove(this.gameObject);
+    }
+}
+
+public enum Fraction
+{
+    Player,
+    Enemy
+}
