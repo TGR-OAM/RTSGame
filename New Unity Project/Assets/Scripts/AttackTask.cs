38bc729fe08f2773c0e79de76d6fee0c79d870f1
YoungFlamer <serhii_iyevlyev@ukr.net>
2020-11-25 16:53:45 +0200

    Units added

---
+++ New Unity Project/Assets/Scripts/AttackTask.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackTask : GameOrder
+{
+    public GameObject target;
+    public AttackTask (GameObject t)
+    {
+        target = t;
+    }
+}
