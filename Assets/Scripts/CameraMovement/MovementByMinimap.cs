b522c4a8220d3347bfeef83aff492d34c37ecea9
Borys <boris233451@gmail.com>
2020-11-28 04:30:24 +0300

    Removed debug.log

--- Assets/Scripts/CameraMovement/MovementByMinimap.cs
+++ Assets/Scripts/CameraMovement/MovementByMinimap.cs
@@ -11,8 +11,6 @@ public class MovementByMinimap : MonoBehaviour
         float relXCursor = (Input.mousePosition.x - (this.transform.position.x - this.GetComponent<RectTransform>().rect.width/2))/ this.GetComponent<RectTransform>().rect.width * hexGrid.MapData.widthInUnits;
         float relYCursor = (Input.mousePosition.y - (this.transform.position.y - this.GetComponent<RectTransform>().rect.height / 2))/ this.GetComponent<RectTransform>().rect.height * hexGrid.MapData.heightInUnits;
 
-        Debug.Log(relXCursor + " " + relYCursor);
-
         Vector3 newPosition = new Vector3(relXCursor, 0, relYCursor);
 
         Camera.main.transform.GetComponent<MovementByKeyBoard>().MoveToHorPositionOnMap(newPosition);
