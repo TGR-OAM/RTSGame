82d0bb8c4e98076065463b37fb2b22ffceab2b7c
YoungFlamer <serhii_iyevlyev@ukr.net>
2020-12-25 19:37:36 +0200

    Resources fixed

---
+++ Assets/Scripts/GameResources/GameResourceStruct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace GameResources
+{
+    [System.Serializable]
+    public struct GameResourceStruct
+    {
+        [SerializeField]
+        public int neoSteel { get; set; }
+        [SerializeField]
+        public int titanium { get; set; }
+        [SerializeField]
+        public int unobtanium { get; set; }
+        public static GameResourceStruct operator +(GameResourceStruct a, GameResourceStruct b)
+        {
+            GameResourceStruct result = new GameResourceStruct();
+            result.neoSteel = a.neoSteel + b.neoSteel;
+            result.titanium = a.titanium + b.titanium;
+            result.unobtanium = a.unobtanium + b.unobtanium;
+            return result;
+        }
+        public static GameResourceStruct operator -(GameResourceStruct a, GameResourceStruct b)
+        {
+            GameResourceStruct result = new GameResourceStruct();
+            result.neoSteel = a.neoSteel - b.neoSteel;
+            result.titanium = a.titanium - b.titanium;
+            result.unobtanium = a.unobtanium - b.unobtanium;
+            return result;
+        }
+    }
+}
